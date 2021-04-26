d337b98be0093af932ec37324edec0fb88eedbef
Eddie Krill <scherenhaenden@gmail.com>
2021-04-26 15:32:32 +0200

    Add first step:
    -Add basic instruction (sum)
    -Add first basic output for user

---
+++ Math/BasicInstructions.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Learning_Project02.Math
+{
+    public class BasicInstructions
+    {
+        public int SumTwoNumbers(int a, int b)
+        {
+            return a + b;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Learning_Project02.Math;
+
+namespace Learning_Project02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Please, insert the first number");
+            var value1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please, insert the second number");
+            var value2 = Convert.ToInt32(Console.ReadLine());
+
+            BasicInstructions basicInstructions = new BasicInstructions();
+            var result = basicInstructions.SumTwoNumbers(value1, value2);
+            Console.WriteLine($"The summatory of the first value " + value1 + " and the second value "+ value2+ " is:");
+            Console.WriteLine(result);
+        }
+    }
+}
